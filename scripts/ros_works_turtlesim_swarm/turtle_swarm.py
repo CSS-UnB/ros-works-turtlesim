1fee6b7bb089226b9a1229fcec2cfe320191d339
Perruci <pedro.perruci@gmail.com>
2018-04-21 22:03:00 -0300

    Eliminacao de tartarugas, ainda em teste

--- scripts/ros_works_turtlesim_swarm/turtle_swarm.py
+++ scripts/ros_works_turtlesim_swarm/turtle_swarm.py
@@ -7,9 +7,18 @@ class TurtleSwarm:
     def __init__(self):
         ''' Construtor da classe '''
         # remove tartaruga inicial da simulacao
-        self.kill_turtle('turtle1') #TODO: TRATAR SE A TURTLE! JA FOI APAGADA
+        turtle.kill_turtle('turtle1') #TODO: TRATAR SE A TURTLE! JA FOI APAGADA
         self.turtleList = []   # lista de tartarugas vivas
         self.menu = Menu()
+
+    def list_turtles(self):
+        ''' Lista nomes das tartarugas '''
+        print 'Turtles in Workspace: '
+        print [turtle.name for turtle in self.turtleList]
+
+    def pop_turtle(self, name):
+        ''' Remove tartaruga da lista '''
+        [self.turtleList.remove(turtle) for turtle in self.turtleList if turtle.name != name]
 
     def create_turtle(self, spawn_name):
         '''Funcao que cria uma nova turtle e a adiciona a turtleList '''
@@ -22,7 +31,7 @@ class TurtleSwarm:
         ''' Funcao que remove uma tartaruga da simulacao '''
         turtle.kill_turtle(victim_name)
         print '{} killed!\n\n'.format(victim_name)
-        # TODO: retirar turtle de turtleList
+        self.pop_turtle(victim_name)
 
     def run(self):
         ''' Funcao principal da TurtleSwarm
@@ -31,9 +40,11 @@ class TurtleSwarm:
         option = self.menu.print_menu()
         if option == 1:
             # Create Turtle
+            self.list_turtles()
             turtle_name = self.menu.get_name('\nChoose your turtle`s name:\n>> ')
             self.create_turtle(turtle_name)
         if option == 2:
             # Kill turtle
+            self.list_turtles()
             turtle_name = self.menu.get_name('\nType the name of turtle you want to kill:\n>> ')
             self.kill_turtle(turtle_name)
